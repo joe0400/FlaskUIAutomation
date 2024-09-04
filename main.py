961cc7749c3ce7520cbf59133842d78d678140a6
Joseph Scannell <joe0400@gmail.com>
2024-09-04 19:46:30 -0400

    created the initial request routing for this, next is to setup the page for requests in/out for this

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 
 
-from flask import Flask
-
+from flask import Flask, request
+import os
+import openai
 
 application = Flask(__name__)
 
-main_page_file = "index.html"
+main_page_file = os.path.join(os.path.join(os.getcwd(),"pages"),"index.html")
 
 @application.route("/")
 def load_page():
     with open(main_page_file,"r") as file:
-        return "".join(file.readlines());
+        return "".join(file.readlines())
 
+
+def register_app_routes():
+
+    for each in os.listdir(os.path.join(os.getcwd(),"pages")):
+        if os.path.isfile(os.path.join(os.path.join(os.getcwd(),"pages"),each)):
+            @application.route(f"/${each}")
+            def handler():
+                with open(os.path.join(os.path.join(os.getcwd(),"pages"),each),"r") as file:
+                    return "".join(file.readlines())
+
+
+@application.route("/settings", methods=['GET','POST'])
+def settings():
+    match request.method:
+        case "GET":
+            return getsettings()
+        case "POST":
+            return modifysettings()
+
+def getsettings():
+    pass
+
+def modifysettings():
+    pass
+
+
+@application.route("/listeners")
+def listen_to_server_port():
+    pass
+
+register_app_routes()
+
+if __name__ == '__main__':
+    application.run()
 
