3f0de31d50f4ca847a6257e10e8ac20e197a890b
Joseph Scannell <joe0400@gmail.com>
2024-09-08 18:57:10 -0400

    added gitignore, some of the initial audio procesisng functionality

--- AudioTranscevier.py
+++ AudioTranscevier.py
@@ -1,14 +1,47 @@
 import pyaudio
 import librosa
 import numpy
+import threading
 
 class AudioTranscevier:
 
     def __init__(self, timestep):
         self.timestep = timestep
+        self.callback = None
+
+    def set_callback(self, callback):
+        self.callback = callback
+
+    def callback_processor(self, in_data, frame_count, time_info, flag):
+        if(self.callback == None): return
+        self.callback(in_data, frame_count, time_info, flag)
 
     def setup_transceiver(self):
+        self.pyaudio_value = pyaudio.PyAudio()
+        self.stream = self.pyaudio_value.open(
+            format=pyaudio.paFloat32,
+            channels=2,
+            rate=44100,
+            input=True,
+            output=False,
+            stream_callback=self.callback_processor,
+            frames_per_buffer=1024*2
+        )
 
-        # alright how much of a buffer is needed for this
+    # this needs to be daemonic in nature, for it to make sense
 
-        raise NotImplemented()
+    def mainloop(self):
+
+        def innerfunc():
+            rolling_frames = []
+            while(True):
+                frame = self.stream.read(1)
+                rolling_frames.append(frame)
+                if(len(rolling_frames) > 10):
+                    rolling_frames = rolling_frames[:-1:10]
+
+        threading.Thread(
+            target=innerfunc,
+            name="AudioProcessorThread",
+            daemon=True
+        )
--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 
 
 sudo apt-get install python3-pip
+sudo apt-get install git git-lfs
+sudo apt-get install portaudio19-dev
+mkdir models
+cd models
+git lfs clone https://huggingface.co/huggyllama/llama-7b
+
+cd ..
+mkdir llama_cpp
+cd llama_cpp
+git clone https://github.com/ggerganov/llama.cpp.git
+cd llama.cpp
+make -j $(nproc --all)
+
+
+mkdir llama_model_server
 
 pip3 install flask openai tacotron pyaudio transformers numpy librosa
 
